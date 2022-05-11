8ef7da919bf5dc95f2ded888f15bfad458092938
Rommy Miranda <rommy.miranda@cirvianum.cat>
2022-05-11 02:50:48 +0200

    commit

--- database/migrations/2022_05_10_193805_create_reservas_table.php
+++ database/migrations/2022_05_10_193805_create_reservas_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('reserves', function (Blueprint $table) {
             $table->id();
+            $table->integer('numPersones');
+            $table->date('dataReserva');
             $table->timestamps();
         });
     }
